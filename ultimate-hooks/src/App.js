c1ec0d4815bbf33924042e1eecd6846c5440d1f2
Marc Pampols <mpampols@mac-mpampols.local>
2021-08-25 16:28:07 +0200

    Add part 7.8

--- ultimate-hooks/src/App.js
+++ ultimate-hooks/src/App.js
@@ -1,39 +1,6 @@
-  
 import React, { useState, useEffect } from 'react'
+import { useField, useResource } from './hooks'
 import axios from 'axios'
-
-
-const useField = (type) => {
-  const [value, setValue] = useState('')
-
-  const onChange = (event) => {
-    setValue(event.target.value)
-  }
-
-  return {
-    type,
-    value,
-    onChange
-  }
-}
-
-const useResource = (baseUrl) => {
-  const [resources, setResources] = useState([])
-
-  // ...
-
-  const create = (resource) => {
-    // ...
-  }
-
-  const service = {
-    create
-  }
-
-  return [
-    resources, service
-  ]
-}
 
 const App = () => {
   const content = useField('text')
